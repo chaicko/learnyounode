4320c1960f67de80d6d009ef2c57c8c07cb3df17
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-12-27 17:51:10 +1100

    solution 11: HTTP file server

---
+++ http-file-server.js
@@ -0,0 +1,12 @@
+'use strict';
+const net = require('net');
+const http = require('http');
+const fs = require('fs');
+const portNumber = process.argv[2];
+const fileToServe = process.argv[3];
+
+let server = http.createServer(function(request, response) {
+    response.writeHead(200, { 'content-type': 'text/plain' });
+    fs.createReadStream(fileToServe).pipe(response);
+});
+server.listen(portNumber);
